221fe3b2f9a46a5cd1b9d92b2eddc0e4cab71a8d
Hussein Kandil <101815486+hussein-m-kandil@users.noreply.github.com>
2025-11-23 13:39:48 +0200

    Configure HTTP client with retrying interceptor

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,13 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { retryingInterceptor } from './retrying-interceptor';
 import { provideRouter } from '@angular/router';
-
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
+    provideHttpClient(withInterceptors([retryingInterceptor])),
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes)
-  ]
+    provideRouter(routes),
+  ],
 };
