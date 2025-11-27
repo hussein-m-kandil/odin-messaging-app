7e6901525403f793a2075aea2dda7d8b82b79eb5
Hussein Kandil <101815486+hussein-m-kandil@users.noreply.github.com>
2025-11-27 12:31:31 +0200

    Add authGuard to control route access

---
+++ src/app/auth/auth-guard.spec.ts
@@ -0,0 +1,114 @@
+import {
+  UrlSegment,
+  CanActivateFn,
+  RedirectCommand,
+  RouterStateSnapshot,
+  ActivatedRouteSnapshot,
+} from '@angular/router';
+import { firstValueFrom, Observable, of } from 'rxjs';
+import { TestBed } from '@angular/core/testing';
+import { authGuard } from './auth-guard';
+import { Auth } from './auth';
+
+const mockAuthService = vi.fn(() => ({ authenticated$: of(true) }));
+
+const setup = () => {
+  TestBed.configureTestingModule({ providers: [{ provide: Auth, useValue: mockAuthService() }] });
+  const executeGuard: CanActivateFn = (...guardParameters) => {
+    return TestBed.runInInjectionContext(() => authGuard(...guardParameters));
+  };
+  return { executeGuard };
+};
+
+const getGuardParameters = (routeUrl: Partial<UrlSegment>[] = [], stateUrl = '/') => {
+  const route = { url: routeUrl } as ActivatedRouteSnapshot;
+  const state = { url: stateUrl } as RouterStateSnapshot;
+  return [route, state] as const;
+};
+
+describe('authGuard', () => {
+  it('should return an observable', () => {
+    const { executeGuard } = setup();
+    const result = executeGuard(...getGuardParameters());
+    expect(result).toBeInstanceOf(Observable);
+  });
+
+  it('should allow an authenticated user to visit a non-auth URL', async () => {
+    mockAuthService.mockImplementation(() => ({ authenticated$: of(true) }));
+    const { executeGuard } = setup();
+    const routeUrls: Partial<UrlSegment>[][] = [
+      [],
+      [{ path: '' }],
+      [{ path: 'foo' }],
+      [{ path: 'bar' }, { path: 'tar' }],
+    ];
+    for (const url of routeUrls) {
+      const guardParameters = getGuardParameters(url);
+      const result = await firstValueFrom(executeGuard(...guardParameters) as Observable<boolean>);
+      expect(result).toBe(true);
+    }
+    mockAuthService.mockReset();
+  });
+
+  it('should disallow an authenticated user to visit an auth URL', async () => {
+    mockAuthService.mockImplementation(() => ({ authenticated$: of(true) }));
+    const { executeGuard } = setup();
+    const routeUrls: Partial<UrlSegment>[][] = [
+      [{ path: 'signin' }],
+      [{ path: 'signup' }],
+      [{ path: 'blah' }, { path: 'signin' }],
+      [{ path: 'blah' }, { path: 'signup' }],
+    ];
+    for (const url of routeUrls) {
+      const stateUrl = `/xyz/${crypto.randomUUID()}`;
+      const guardParameters = getGuardParameters(url, stateUrl);
+      const result = await firstValueFrom(
+        executeGuard(...guardParameters) as Observable<RedirectCommand>
+      );
+      expect(result).toBeInstanceOf(RedirectCommand);
+      expect(result.redirectTo.toString()).toBe('/');
+      expect(result.redirectTo.queryParams).toStrictEqual({});
+    }
+    mockAuthService.mockReset();
+  });
+
+  it('should allow an unauthenticated user to visit an auth URL', async () => {
+    mockAuthService.mockImplementation(() => ({ authenticated$: of(false) }));
+    const { executeGuard } = setup();
+
+    const routeUrls: Partial<UrlSegment>[][] = [
+      [{ path: 'signin' }],
+      [{ path: 'signup' }],
+      [{ path: 'blah' }, { path: 'signin' }],
+      [{ path: 'blah' }, { path: 'signup' }],
+    ];
+    for (const url of routeUrls) {
+      const guardParameters = getGuardParameters(url);
+      const result = await firstValueFrom(executeGuard(...guardParameters) as Observable<boolean>);
+      expect(result).toBe(true);
+    }
+    mockAuthService.mockReset();
+  });
+
+  it('should disallow an unauthenticated user to visit a non-auth URL', async () => {
+    mockAuthService.mockImplementation(() => ({ authenticated$: of(false) }));
+    const { executeGuard } = setup();
+    const routeUrls: Partial<UrlSegment>[][] = [
+      [],
+      [{ path: '' }],
+      [{ path: 'foo' }],
+      [{ path: 'bar' }, { path: 'tar' }],
+    ];
+    for (const url of routeUrls) {
+      const stateUrl = `/xyz/${crypto.randomUUID()}`;
+      const guardParameters = getGuardParameters(url, stateUrl);
+      const result = await firstValueFrom(
+        executeGuard(...guardParameters) as Observable<RedirectCommand>
+      );
+      expect(result).toBeInstanceOf(RedirectCommand);
+      expect(result.redirectTo.toString()).toMatch(/\/signin\?.*$/);
+      expect(result.redirectTo.queryParams).toStrictEqual({ url: stateUrl });
+    }
+    mockAuthService.mockReset();
+  });
+});
---
+++ src/app/auth/auth-guard.ts
@@ -0,0 +1,25 @@
+import { CanActivateFn, RedirectCommand, Router } from '@angular/router';
+import { authNavOpts, isAuthUrl } from './utils';
+import { inject } from '@angular/core';
+import { Auth } from './auth';
+import { map } from 'rxjs';
+
+export const authGuard: CanActivateFn = (route, state) => {
+  const router = inject(Router);
+  const auth = inject(Auth);
+  return auth.authenticated$.pipe(
+    map((authenticated) => {
+      const authenticating = isAuthUrl(route.url);
+      let redirectCommand: RedirectCommand | undefined;
+      if (authenticating && authenticated) {
+        redirectCommand = new RedirectCommand(router.createUrlTree(['/']), authNavOpts);
+      } else if (!authenticating && !authenticated) {
+        redirectCommand = new RedirectCommand(
+          router.createUrlTree(['/signin'], { queryParams: { url: state.url } }),
+          authNavOpts
+        );
+      }
+      return redirectCommand || true;
+    })
+  );
+};
