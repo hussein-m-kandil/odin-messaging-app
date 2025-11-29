2bc30754f676980edae32cef34ce8d76d3cd0ae1
Hussein Kandil <101815486+hussein-m-kandil@users.noreply.github.com>
2025-11-29 11:44:37 +0200

    Show sign-out button only for authenticated users

--- src/app/app.spec.ts
+++ src/app/app.spec.ts
@@ -1,11 +1,17 @@
-import { render, screen } from '@testing-library/angular';
+import { render, screen, waitForElementToBeRemoved } from '@testing-library/angular';
+import { userEvent } from '@testing-library/user-event';
 import { provideRouter } from '@angular/router';
 import { environment } from '../environments';
+import { Auth } from './auth';
 import { App } from './app';
+import { of } from 'rxjs';
+
+const signOut = vi.fn();
+const mockAuthService = vi.fn(() => ({ authenticated$: of(true), signOut }));
 
 const renderComponent = () => {
   return render(App, {
-    providers: [provideRouter([])],
+    providers: [{ provide: Auth, useValue: mockAuthService() }, provideRouter([])],
   });
 };
 
@@ -18,5 +24,37 @@ describe('App', () => {
   it('should show loader on navigation', async () => {
     await renderComponent();
     expect(screen.getByText(/loading/i)).toBeVisible();
+  });
+
+  it('should not show sign-out button for an unauthenticated user', async () => {
+    mockAuthService.mockImplementationOnce(() => ({
+      ...mockAuthService(),
+      authenticated$: of(false),
+    }));
+    await renderComponent();
+    await waitForElementToBeRemoved(screen.getByText(/loading/i));
+    expect(screen.queryByRole('button', { name: /sign ?out/i })).toBeNull();
+  });
+
+  it('should show sign-out button for an authenticated user', async () => {
+    mockAuthService.mockImplementationOnce(() => ({
+      ...mockAuthService(),
+      authenticated$: of(true),
+    }));
+    await renderComponent();
+    await waitForElementToBeRemoved(screen.getByText(/loading/i));
+    expect(screen.getByRole('button', { name: /sign ?out/i })).toBeVisible();
+  });
+
+  it('should call `signOut` function from the Auth service when clicking the sign-out button', async () => {
+    mockAuthService.mockImplementationOnce(() => ({
+      ...mockAuthService(),
+      authenticated$: of(true),
+    }));
+    const user = userEvent.setup();
+    await renderComponent();
+    await waitForElementToBeRemoved(screen.getByText(/loading/i));
+    await user.click(screen.getByRole('button', { name: /sign ?out/i }));
+    expect(signOut).toHaveBeenCalledOnce();
   });
 });
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,6 +1,8 @@
-import { Router, RouterOutlet } from '@angular/router';
 import { Component, computed, inject } from '@angular/core';
+import { Router, RouterOutlet } from '@angular/router';
+import { toSignal } from '@angular/core/rxjs-interop';
 import { environment } from '../environments';
+import { Auth } from './auth';
 
 @Component({
   selector: 'app-root',
@@ -10,8 +12,15 @@ import { environment } from '../environments';
 })
 export class App {
   private readonly _router = inject(Router);
+  private readonly _auth = inject(Auth);
+
+  protected readonly authenticated = toSignal(this._auth.authenticated$, { initialValue: false });
 
   protected readonly navigating = computed(() => !!this._router.currentNavigation());
 
   protected readonly title = environment.title;
+
+  protected signOut() {
+    this._auth.signOut();
+  }
 }
