87e8ca223d8983fdfb4df9e80e32476458fdec92
Hussein Kandil <101815486+hussein-m-kandil@users.noreply.github.com>
2025-11-29 09:55:47 +0200

    Add sign-in, sign-up, and temp home pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,24 @@
+import { environment } from '../environments';
+import { authGuard } from './auth/auth-guard';
+import { AuthForm } from './auth/auth-form';
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { Component } from '@angular/core';
+
+const { title } = environment;
+const titleize = (s: string) => `${s} | ${title}`;
+
+@Component({ template: '<h1 class="text-center my-4 italic">Temporary Home Page</h1>' })
+class TEMP_HOME {}
+
+export const routes: Routes = [
+  { path: '', component: TEMP_HOME, canActivate: [authGuard] },
+  {
+    path: '',
+    canActivateChild: [authGuard],
+    children: [
+      { path: 'signin', component: AuthForm, title: titleize('Sing In') },
+      { path: 'signup', component: AuthForm, title: titleize('Sing Up') },
+    ],
+  },
+];
