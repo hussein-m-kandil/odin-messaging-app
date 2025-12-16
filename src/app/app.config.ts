6e0959efadcfe61319b038c38610de702c477247
Hussein Kandil <101815486+hussein-m-kandil@users.noreply.github.com>
2025-12-16 13:35:19 +0200

    Provide router with component input binding

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,14 +1,14 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { provideRouter, withComponentInputBinding } from '@angular/router';
 import { retryingInterceptor } from './retrying-interceptor';
 import { authInterceptor } from './auth/auth-interceptor';
-import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideHttpClient(withInterceptors([retryingInterceptor, authInterceptor])),
+    provideRouter(routes, withComponentInputBinding()),
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes),
   ],
 };
