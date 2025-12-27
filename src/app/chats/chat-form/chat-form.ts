8e6b4d0cffb2bd73d08958bbc6a887e3c73718a0
Hussein Kandil <101815486+hussein-m-kandil@users.noreply.github.com>
2025-12-27 17:40:51 +0200

    Extract Home component from App

---
+++ src/app/chats/chat-form/chat-form.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-chat-form',
+  imports: [],
+  templateUrl: './chat-form.html',
+  styles: ``,
+})
+export class ChatForm {
+
+}
